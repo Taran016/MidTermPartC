7db984b972291f3d8ef58e1b4d263713a4dd53e9
Taranpreet Singh <sin14121@sheridancollege.ca>
2024-02-21 09:48:35 -0500

    Added code and TestUserprofile class

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Taranpreet Singh
+ */
+public class TestUserprofile 
+{
+    public static void main(String[] args)
+    {
+    System.out.println("Taranpreet Singh");
+    System.out.println("Favourite Gerne : Mystery");
+    System.out.println("Profile created");
+
+    }
+}
